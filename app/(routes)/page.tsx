6fb2f274fe60aa9552288e830d1ae7e4fa6254b4
Guillaume Flambard <g.flambard@gmail.com>
2024-04-24 15:39:27 +0200

    update billboard test

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
 
-  const billboard = await getBillboard("clvdkp8xe00011hppgamdlqi1");
+  const billboard = await getBillboard("clvduw7lh00026bpb2bq0z4kz");
 
   return (
     <Container>
